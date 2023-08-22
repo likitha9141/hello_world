96ef255883c9898aa66594fc040790d6f3f8f5c2
likitha9141 <140776175+likitha9141@users.noreply.github.com>
2023-08-22 21:41:05 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,6 +3,8 @@
 class sample {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("Hello, World!"); System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
